84e2ddef5b61e34efe541114036d1a9fde101a38
Mostakimul Karim <mostakimulkarim@gmail.com>
2024-08-15 21:29:37 -0400

    [fix]: background position

--- src/components/latest/LatestRecipes.tsx
+++ src/components/latest/LatestRecipes.tsx
@@ -9,18 +9,24 @@ const LatestRecipes = () => {
       <div className="grid grid-cols-1 md:grid-cols-4 gap-5">
         <div
           className="row-span-2 grid place-content-center text-center py-10 md:py-0"
-          style={{ backgroundImage: "url('/side-bg.jpg')" }}
+          style={{
+            backgroundImage: "url('/side-bg.jpg')",
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+          }}
         >
-          <h3 className="font-semibold text-gray-100 text-center text-4xl md:text-6xl">
-            Latest Recipes
-          </h3>
-          <div className="mt-10 mb-10 sm:mb-0">
-            <Link
-              href={'/'}
-              className="btn px-10 rounded-full bg-white shadow-md"
-            >
-              View Recipes <PiCaretCircleRightThin className="text-xl" />
-            </Link>
+          <div className="backdrop-blur-sm h-full w-full py-10">
+            <h3 className="font-semibold text-white text-center text-4xl md:text-6xl">
+              Latest Recipes
+            </h3>
+            <div className="mt-10 mb-10 sm:mb-0">
+              <Link
+                href={'/'}
+                className="btn px-10 rounded-full bg-white shadow-md"
+              >
+                View Recipes <PiCaretCircleRightThin className="text-xl" />
+              </Link>
+            </div>
           </div>
         </div>
         {recipes.map((recipe) => (
